18bb149b516564a4bf7e86b450a82b9d9179a2c1
Symplice <intwarisymplice@gmail.com>
2023-06-17 22:09:44 +0200

    feat: creating social media section

---
+++ src/Components/Data/FollowUpData.js
@@ -0,0 +1,32 @@
+import { AiOutlineInstagram } from 'react-icons/ai'
+import { AiFillYoutube } from 'react-icons/ai'
+import { AiFillTwitterCircle } from 'react-icons/ai'
+import { FaTiktok } from 'react-icons/fa'
+import { AiFillFacebook } from 'react-icons/ai'
+export const FollowUpData = [
+    {
+        FollowLogo: <AiOutlineInstagram />,
+        Subscribed: '',
+        FollowType: '',
+    },
+    {
+        FollowLogo: <AiFillYoutube />,
+        Subscribed: '',
+        FollowType: '',
+    },
+    {
+        FollowLogo: <AiFillTwitterCircle />,
+        Subscribed: '',
+        FollowType: '',
+    },
+    {
+        FollowLogo: <FaTiktok />,
+        Subscribed: '',
+        FollowType: '',
+    },
+    {
+        FollowLogo: <AiFillFacebook />,
+        Subscribed: '',
+        FollowType: '',
+    }
+]

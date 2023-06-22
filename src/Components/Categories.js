1a0dcb23f7823c1e8013e98e42afacfa0e192a77
symplice-nunu <intwarisymplice@gmail.com>
2023-06-22 17:02:02 +0530

    feat: displaying news categories on HomePage

--- src/Components/Categories.js
+++ src/Components/Categories.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { CategoriesData } from './Data/CategoriesData'
 
 export default function Categories() {
   return (
@@ -7,6 +8,21 @@ export default function Categories() {
   <div className='flex'>
     <div className='h-[2px] mt-2 bg-[#1d96fb] w-12'></div>
     <div className='h-[2px] mt-2 bg-[#eeeeee] w-full'></div>
+  </div>
+  <div className='mt-7'>
+  {
+    CategoriesData.map((item) => {
+        return (
+            <div>
+            <div className='flex justify-between mb-2'>
+    <div className='font-bold font-[Heebo] text-[19px]'>{item.CategoryName}</div>
+    <div className='text-[#a0a0a0]'>({item.CategoryNumber})</div>
+  </div>
+  <div className='h-[2px] mb-2 bg-[#eeeeee] w-full'></div>
+        </div>
+        )
+    })
+  }
   </div>
     </div>
   )

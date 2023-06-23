4335395d4674ff66d1400addbfcb7404305b0d60
symplice-nunu <intwarisymplice@gmail.com>
2023-06-23 17:20:51 +0530

    feat: creating business top news

--- src/Components/Business/BusinessTopNews.js
+++ src/Components/Business/BusinessTopNews.js
@@ -24,7 +24,7 @@ export default function BusinessTopNews() {
       <div className='w-[0px]'></div>
   </div>
   <div className='space-y-[2px]'>
-  <div className="bg-teal-400  flex items-end w-[790px] h-[350px] pb-[30px] px-[15px] text-white  bg-[url('https://jnews.io/bloomnews/wp-content/uploads/sites/75/2020/02/05-750x375.jpg')] bg-cover bg-center" >
+  <div className="bg-teal-400  flex items-end w-[790px] h-[350px] pb-[30px] px-[15px] text-white  bg-[url('https://jnews.io/bloomnews/wp-content/uploads/sites/75/2020/01/19-750x375.jpg')] bg-cover bg-center" >
   <div className='w-full'>
       <div className='bg-[#fe495c] rounded font-[heebo] text-[11px] py-[4px] px-[7px] w-[66px]'>BUSINESS</div>
       <div className='text-[18px] font-[Poppins] py-3'>Instagram Is Testing Photo Albums, Because Nothing is Sacred Anymore</div>
@@ -39,7 +39,7 @@ export default function BusinessTopNews() {
       <div className='w-[0px]'></div>
   </div>
   <div className='space-y-[2px] flex gap-[2px] '>
-  <div className=" bg-blue-300 h-[230px] flex items-end pb-[30px] px-[15px] text-white bg-[url('https://jnews.io/bloomnews/wp-content/uploads/sites/75/2020/02/07-750x375.jpg')] bg-cover bg-center">
+  <div className=" bg-blue-300 h-[230px] flex items-end pb-[30px] px-[15px] text-white bg-[url('https://jnews.io/bloomnews/wp-content/uploads/sites/75/2020/02/05-750x375.jpg')] bg-cover bg-center">
   <div className='w-full'>
       <div className='bg-[#9f4bc3] rounded font-[heebo] text-[11px] py-[4px] px-[7px] w-[108px]'>ENTERTAINMENT</div>
       <div className='text-[18px] font-[Poppins] py-3'>Instagram Is Testing Photo Albums, Because Nothing is Sacred Anymore</div>
@@ -53,7 +53,7 @@ export default function BusinessTopNews() {
       </div>
       <div className='w-[0px]'></div>
   </div>
-  <div className="  bg-blue-300 h-[230px] flex items-end pb-[30px] px-[15px] text-white bg-[url('https://jnews.io/bloomnews/wp-content/uploads/sites/75/2020/01/11-750x375.jpg')] bg-cover bg-center bg-no-repeat">
+  <div className="  bg-blue-300 h-[230px] flex items-end pb-[30px] px-[15px] text-white bg-[url('https://jnews.io/bloomnews/wp-content/uploads/sites/75/2020/05/25-750x375.jpg')] bg-cover bg-center bg-no-repeat">
   <div className='w-full'>
       <div className='bg-[#c49529] rounded font-[heebo] text-[11px] py-[4px] px-[7px] w-[63px]'>FASHION</div>
       <div className='text-[18px] font-[Poppins] py-3'>Instagram Is Testing Photo Albums, Because Nothing is Sacred Anymore</div>
@@ -76,15 +76,15 @@ export default function BusinessTopNews() {
     </Link>    
     <div className='mt-[5px] text-[#b2acb5]'><TbMathGreater /></div>
 
-    <Link to="/world">
+    <Link to="/business">
     <div className='hover:text-[#1d96fb]'>Category</div>
     </Link>
     <div className='mt-[5px] text-[#b2acb5]'><TbMathGreater /></div>
-    <Link to="/world">
-    <div className='text-[#b2acb5] hover:text-[#1d96fb]'>World</div>
+    <Link to="/business">
+    <div className='text-[#b2acb5] hover:text-[#1d96fb]'>Business</div>
     </Link>
   </div>
-  <div className='font-bold text-[60px] font-[Heebo]'>World</div>
+  <div className='font-bold text-[60px] font-[Heebo]'>Business</div>
   <div className='flex gap-4'>
     <div>
     <TopLatestPost />
